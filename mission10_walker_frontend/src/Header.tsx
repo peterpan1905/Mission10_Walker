732fdddde1e93acd1ae930d01c4f1edb7e9b929d
peterpan1905 <mrpiercewalker@gmail.com>
2024-03-12 16:58:35 -0600

    Added Header.tsx

---
+++ mission10_walker_frontend/src/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="row">
+      <div className="col-4">
+        <h1>Bowling League Data</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
