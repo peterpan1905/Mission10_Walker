99ad61e4d1c0963990f3aff11e833ab88de3d5fd
peterpan1905 <mrpiercewalker@gmail.com>
2024-03-12 19:34:34 -0600

    Changed some stuff on table

--- mission10_walker_frontend/src/Bowlers/BowlerList.tsx
+++ mission10_walker_frontend/src/Bowlers/BowlerList.tsx
@@ -4,16 +4,19 @@ function BowlerList() {
       <div className="row">
         <h4 className="text-center">Bowlers</h4>
       </div>
-      <table className="table table-boredered">
-        <tr>
-          <th>Bowler Name</th>
-          <th>Team Name</th>
-          <th>Address</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Zip</th>
-          <th>Phone Number</th>
-        </tr>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Bowler Name</th>
+            <th>Team Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
       </table>
     </>
   );
